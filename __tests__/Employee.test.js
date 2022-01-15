d7a15e947ea0c22ac13304bfe1207cc23806d838
Daniel <ddoherty6@gmail.com>
2022-01-14 20:09:46 -0800

    added classes and employee test

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,17 @@
+const Employee = require('../src/Employee');
+
+test('Employee constructor function has all required properties', () => {
+    const employee = new Employee('Jim Halpert', "jim.halpert@office.net");
+
+    // testing static properties
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.any(String));
+    expect(employee.role).toEqual(expect.stringContaining("Employee"));
+
+    //testing methods
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name));
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email));
+    expect(employee.getRole()).toEqual(expect.stringContaining(employee.role));
+});
